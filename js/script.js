1230d8957d1b15567698f369d1d0d096f162f040
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-23 14:44:18 +0100

    completamento dell'esercizio 'Countdown'

--- js/script.js
+++ js/script.js
@@ -12,5 +12,19 @@ const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
     const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
-   
+    // inserisco i dati nell'output
+    document.getElementById('days').innerHTML = days + 'd '
+    document.getElementById('hours').innerHTML = hours + 'h '
+    document.getElementById('mins').innerHTML = minutes + 'm ' 
+    document.getElementById('secs').innerHTML = seconds + 's ' 
+        
+    // aggiungo il messaggio al termine del countdown
+    if (timeleft < 0) {
+        clearInterval(myFunction);
+        document.getElementById("days").innerHTML = ''
+        document.getElementById("hours").innerHTML = ''
+        document.getElementById("mins").innerHTML = ''
+        document.getElementById("secs").innerHTML = ''
+        document.getElementById("end").innerHTML = 'HAPPY NEW YEAR!!';
+    }
     }, 1000);
